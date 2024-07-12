39054a6bcc588d7fb7b3e94082351f0951c17bbf
Manahil rasheed <manahilrasheed507@gmail.com>
2024-07-12 08:03:30 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Strongly type syntax
+var a = "Pakistan";
+a = "USA";
+var b = 9;
+var c = true;
+//Type infrence 
+var e = "USA";
+var f = 10.9;
+f = 22;
+var g = false;
+g = true;
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Strongly type syntax
+let a: String = "Pakistan";
+a = "USA"
+let b: number = 9
+let c: boolean = true
+
+//Type infrence 
+let e = "USA"
+let f = 10.9
+f = 22
+let g = false;
+g = true
